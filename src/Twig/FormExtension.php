907a53b568479a6e9da5e9b0d942d16b66356819
weeger <romain.weeger@gmail.com>
2026-01-19 10:12:59 +0100

    Forms

--- src/Twig/FormExtension.php
+++ src/Twig/FormExtension.php
@@ -31,81 +31,11 @@ class FormExtension extends \Wexample\SymfonyDesignSystem\Twig\AbstractTemplateE
 
     private function normalizeInputContext(array $context): array
     {
-        if (isset($context['schema']) && is_array($context['schema'])) {
-            $schema = $context['schema'];
-
-            if (!isset($context['label']) && isset($schema['title'])) {
-                $context['label'] = $schema['title'];
-            }
-
-            if (!isset($context['placeholder']) && isset($schema['description'])) {
-                $context['placeholder'] = $schema['description'];
-            }
-
-            if (!isset($context['type']) && isset($schema['type'])) {
-                $context['type'] = $this->mapSchemaType($schema);
-            }
-
-            if (!isset($context['required'])) {
-                $context['required'] = $this->inferRequired($schema);
-            }
-
-            if (!isset($context['value']) && array_key_exists('default', $schema)) {
-                $context['value'] = $schema['default'];
-            }
-        }
-
         if (!isset($context['id']) && isset($context['name'])) {
             $context['id'] = $context['name'];
         }
 
-        if (!isset($context['type'])) {
-            $context['type'] = 'text';
-        }
-
         return $context;
-    }
-
-    private function inferRequired(array $schema): bool
-    {
-        if (isset($schema['required']) && $schema['required'] === true) {
-            return true;
-        }
-
-        if (isset($schema['minLength']) && is_int($schema['minLength'])) {
-            return $schema['minLength'] > 0;
-        }
-
-        return false;
-    }
-
-    private function mapSchemaType(array $schema): string
-    {
-        $type = $schema['type'] ?? 'string';
-        $format = $schema['format'] ?? null;
-
-        if ($type === 'string' && is_string($format)) {
-            $formatMap = [
-                'email' => 'email',
-                'uri' => 'url',
-                'date' => 'date',
-                'date-time' => 'datetime-local',
-            ];
-
-            if (isset($formatMap[$format])) {
-                return $formatMap[$format];
-            }
-        }
-
-        if ($type === 'integer' || $type === 'number') {
-            return 'number';
-        }
-
-        if ($type === 'boolean') {
-            return 'checkbox';
-        }
-
-        return 'text';
     }
 
     private function validateInputContext(array $context): void
@@ -123,11 +53,9 @@ class FormExtension extends \Wexample\SymfonyDesignSystem\Twig\AbstractTemplateE
             ));
         }
 
-        $schemaArray = json_decode($schemaJson, true, 512, JSON_THROW_ON_ERROR);
         $schema = Schema::fromJsonString($schemaJson);
 
-        $validatedKeys = array_keys($schemaArray['properties'] ?? []);
-        $data = array_intersect_key($context, array_flip($validatedKeys));
+        $data = $this->stripTwigContext($context);
         $dataObject = json_decode(json_encode($data, JSON_THROW_ON_ERROR));
 
         $validator = new Validator();
@@ -148,6 +76,23 @@ class FormExtension extends \Wexample\SymfonyDesignSystem\Twig\AbstractTemplateE
                 $message
             ));
         }
+    }
+
+    private function stripTwigContext(array $context): array
+    {
+        $excludeKeys = [
+            'id',
+            'value_attr',
+            'input_attr',
+            'error_condition',
+            'error_text',
+        ];
+
+        foreach ($excludeKeys as $key) {
+            unset($context[$key]);
+        }
+
+        return $context;
     }
 
     private function getInputSchemaPath(): string
