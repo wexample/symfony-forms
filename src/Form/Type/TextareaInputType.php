61c4617c92e242ddb69daac820bb32512718f48e
weeger <romain.weeger@gmail.com>
2026-01-30 10:11:33 +0100

    Textarea

--- src/Form/Type/TextareaInputType.php
+++ src/Form/Type/TextareaInputType.php
@@ -2,6 +2,9 @@
 
 namespace Wexample\SymfonyForms\Form\Type;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Wexample\SymfonyForms\Form\Traits\FieldOptionsTrait;
 
 class TextareaInputType extends \Symfony\Component\Form\AbstractType
@@ -16,5 +19,21 @@ class TextareaInputType extends \Symfony\Component\Form\AbstractType
     public function getBlockPrefix(): string
     {
         return 'textarea_input';
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'rows' => null,
+        ]);
+        $resolver->setAllowedTypes('rows', ['null', 'int']);
+    }
+
+    public function buildView(
+        FormView $view,
+        FormInterface $form,
+        array $options
+    ): void {
+        $view->vars['rows'] = $options['rows'];
     }
 }
