ff1280009aa6ff185af6d13128029d58eb7a34d4
weeger <romain.weeger@gmail.com>
2026-01-30 14:35:44 +0100

    Select form item

--- src/Form/Demo/FormTextSimpleForm.php
+++ src/Form/Demo/FormTextSimpleForm.php
@@ -4,6 +4,7 @@ namespace Wexample\SymfonyForms\Form\Demo;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Wexample\SymfonyForms\Form\AbstractForm;
+use Wexample\SymfonyForms\Form\Type\SelectInputType;
 use Wexample\SymfonyForms\Form\Type\TextareaInputType;
 use Wexample\SymfonyForms\Form\Type\TextInputType;
 
@@ -25,6 +26,23 @@ class FormTextSimpleForm extends AbstractForm
             );
 
         $builder
+            ->add(
+                'text_select',
+                SelectInputType::class,
+                [
+                    self::FIELD_OPTION_NAME_LABEL => true,
+                    self::FIELD_OPTION_NAME_REQUIRED => false,
+                    self::FIELD_OPTION_NAME_MAPPED => false,
+                    'choices' => [
+                        'First choice' => 'first',
+                        'Second choice' => 'second',
+                        'Group' => [
+                            'Third choice' => 'third',
+                        ],
+                    ],
+                    'placeholder' => true,
+                ]
+            )
             ->add(
                 'text_area',
                 TextareaInputType::class,
