4ceb97cf06e01b506b5ff43aa046eed13ee4cf03
weeger <romain.weeger@gmail.com>
2026-02-12 16:26:21 +0100

    Select aut translation

--- src/Form/Type/SelectInputType.php
+++ src/Form/Type/SelectInputType.php
@@ -2,6 +2,12 @@
 
 namespace Wexample\SymfonyForms\Form\Type;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
+use Symfony\Component\Form\ChoiceList\View\ChoiceGroupView;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Wexample\SymfonyForms\Form\Traits\FieldOptionsTrait;
 
 class SelectInputType extends \Symfony\Component\Form\AbstractType
@@ -16,5 +22,72 @@ class SelectInputType extends \Symfony\Component\Form\AbstractType
     public function getBlockPrefix(): string
     {
         return 'select_input';
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('auto_translate_choices', true);
+        $resolver->setNormalizer('choices', function (Options $options, $choices) {
+            if (!is_array($choices) || !array_is_list($choices)) {
+                return $choices;
+            }
+
+            $map = [];
+            foreach ($choices as $choice) {
+                if (!is_string($choice)) {
+                    $map[$choice] = $choice;
+                    continue;
+                }
+                $map[$choice] = $choice;
+            }
+
+            return $map;
+        });
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        parent::buildView($view, $form, $options);
+
+        if (empty($options['auto_translate_choices'])) {
+            return;
+        }
+
+        $fieldName = $form->getName();
+        $prefix = '@form::field.' . $fieldName . '.choice.';
+
+        $applyLabel = static function (ChoiceView $choice) use ($prefix): void {
+            $label = $choice->label ?? '';
+            $value = $choice->value ?? '';
+
+            if (!is_string($label) || !is_string($value)) {
+                return;
+            }
+
+            if ($label === '' || $label[0] === '@') {
+                return;
+            }
+
+            if ($label !== $value) {
+                return;
+            }
+
+            $choice->label = $prefix . $value . '.label';
+        };
+
+        foreach ($view->vars['choices'] ?? [] as $choice) {
+            if ($choice instanceof ChoiceGroupView) {
+                foreach ($choice->choices as $groupChoice) {
+                    if ($groupChoice instanceof ChoiceView) {
+                        $applyLabel($groupChoice);
+                    }
+                }
+                continue;
+            }
+
+            if ($choice instanceof ChoiceView) {
+                $applyLabel($choice);
+            }
+        }
     }
 }
