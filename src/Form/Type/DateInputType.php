e194ee95473ff76b06078c9bfb096f3074e80e51
weeger <romain.weeger@gmail.com>
2026-07-25 20:02:32 +0200

    Cleanup

---
+++ src/Form/Type/DateInputType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Wexample\SymfonyForms\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Wexample\SymfonyForms\Form\Traits\FieldOptionsTrait;
+
+class DateInputType extends DateType
+{
+    use FieldOptionsTrait;
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults([
+            'widget' => 'single_text',
+            'html5'  => true,
+        ]);
+    }
+}
---
+++ src/Form/Type/DatetimeInputType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Wexample\SymfonyForms\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Wexample\SymfonyForms\Form\Traits\FieldOptionsTrait;
+
+class DatetimeInputType extends DateTimeType
+{
+    use FieldOptionsTrait;
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults([
+            'widget' => 'single_text',
+            'html5'  => true,
+        ]);
+    }
+}
