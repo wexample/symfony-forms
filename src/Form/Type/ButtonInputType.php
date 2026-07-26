c00240f3a7cfa12643b5a03eeefe9a5af12a8469
weeger <romain.weeger@gmail.com>
2026-07-26 10:42:55 +0200

    Cleanup

--- src/Form/Type/ButtonInputType.php
+++ src/Form/Type/ButtonInputType.php
@@ -8,8 +8,9 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Wexample\SymfonyForms\Form\Traits\FieldOptionsTrait;
+use Symfony\Component\Form\SubmitButtonTypeInterface;
 
-class ButtonInputType extends AbstractType
+class ButtonInputType extends AbstractType implements SubmitButtonTypeInterface
 {
     use FieldOptionsTrait;
 
