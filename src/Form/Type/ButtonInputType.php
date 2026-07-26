784264a3aea92ae39508cac220b293e24c6e48aa
weeger <romain.weeger@gmail.com>
2026-07-26 10:01:34 +0200

    Cleanup

--- src/Form/Type/ButtonInputType.php
+++ src/Form/Type/ButtonInputType.php
@@ -2,10 +2,35 @@
 
 namespace Wexample\SymfonyForms\Form\Type;
 
-class ButtonInputType extends SubmitInputType
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Wexample\SymfonyForms\Form\Traits\FieldOptionsTrait;
+
+class ButtonInputType extends AbstractType
 {
-    public function getBlockPrefix(): string
+    use FieldOptionsTrait;
+
+    public function getParent(): string
     {
-        return 'button_input';
+        return ButtonType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'icon' => null,
+        ]);
+        $resolver->setAllowedTypes('icon', ['null', 'string']);
+    }
+
+    public function buildView(
+        FormView $view,
+        FormInterface $form,
+        array $options
+    ): void {
+        $view->vars['icon'] = $options['icon'];
     }
 }
